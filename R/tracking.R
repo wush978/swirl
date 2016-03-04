3593e96aa3bd4ac0114226e2375535c207db8cdc
Wush Wu <wush978@gmail.com>
2016-03-04 21:56:55 +0800

    multiple tracking ip

--- R/tracking.R
+++ R/tracking.R
@@ -1,3 +1,5 @@
+.tracking_env <- environment()
+
 #'@importFrom httr POST stop_for_status
 tracking <- function(ip, courseName, lessonName, userName, version, type) {
   body <- list(
@@ -7,9 +9,26 @@ tracking <- function(ip, courseName, lessonName, userName, version, type) {
     version = version
   )
   tryCatch({
-    res <- POST(url = sprintf("http://%s:3000/api/status", ip), body = body, encode = "json")
-    stop_for_status(res)
-    message(sprintf("Your status has beed updated to tracking server"))
+    # get the last vailable ip or pass
+    last_ip <- unique(append(.tracking_env[[ip]], strsplit(ip, ",")[[1]]))
+    is_tracked <- FALSE
+    .tracking_env$error_msg <- c()
+    for(current_ip in last_ip) {
+      tryCatch({
+        res <- POST(url = sprintf("http://%s:3000/api/status", current_ip), body = body, encode = "json")
+        stop_for_status(res)
+        is_tracked <- TRUE
+        break
+      }, error = function(e) {
+        .tracking_env$error_msg <- append(.tracking_env$error_msg, sprintf("%s : %s", current_ip, conditionMessage(e)))
+      })
+    }
+    if (is_tracked) {
+      message(sprintf("Your status has beed updated to tracking server"))
+      .tracking_env[[ip]] <- current_ip
+    } else {
+      stop(paste(.tracking_env$error_msg, collapse = "---"))
+    }
   }, error = function(e) {
     warning(sprintf("Failed to communicate with tracking server. The error message is: (%s)", conditionMessage(e)))
   })
